93dbdebdc17099a9d085b85b2c06db6be99b14f0
jvandergraaf <jvgraaf@gmail.com>
2022-08-24 19:05:42 -0600

    Hook up openweather api to nextjs

--- src/pages/[city].tsx
+++ src/pages/[city].tsx
@@ -1,52 +1,37 @@
-import { useRouter } from "next/router"
 import Head from "next/head";
 import { GetServerSideProps, InferGetServerSidePropsType } from 'next'
 import { IndexScreen } from "../components/screens/IndexScreen/IndexScreen";
-import { trpc } from "../utils/trpc";
+import moment from 'moment'
 
 // This gets called on every request
 export const getServerSideProps: GetServerSideProps = async (context) => {
-
   // Fetch data from external API
-  //const res = await fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&appid=e087342eca2f4ee4605ce0db0e3b8fb2&units=metric&exclude=hourly,minutely,alerts`)
-  //const res = await fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&exclude=hourly,minutely,alerts&appid=${process.env.API_KEY}&units=metric`)
-  //const data = await res.json()
-
-
+  const res = await fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&exclude=hourly,minutely,alerts&appid=${process.env.API_KEY}&units=metric`)
+  const data = await res.json()
   // Pass data to the page via props
   return {
     props: {
       cityNav: {
         selectedCity: context.params.city,
-        cities: ["Ottawa", "Moscow", "Tokyo"]
+        cities: ["Bangalore", "Cairo", "Bogota"]
       },
       weather: {
         currentWeather: {
-          weatherTypeText: "Cloudy",
-          iconSrc: "http://openweathermap.org/img/wn/10d@4x.png",
-          temperature: "19"
+          weatherTypeText: data.current.weather[0].main,
+          iconSrc: `http://openweathermap.org/img/wn/${data.current.weather[0].icon}@4x.png`,
+          temperature: `${Math.round(data.current.temp)}`
         },
         futureWeather: {
-          forecast: [{
-            weekday: "Wed",
-            iconSrc: "http://openweathermap.org/img/wn/10d@2x.png",
-            temperature: "-18",
-          },
-          {
-            weekday: "Thu",
-            iconSrc: "http://openweathermap.org/img/wn/11d@2x.png",
-            temperature: "0",
-          },
-          {
-            weekday: "Fri",
-            iconSrc: "http://openweathermap.org/img/wn/13d@2x.png",
-            temperature: "6",
-          },
-          {
-            weekday: "Sat",
-            iconSrc: "http://openweathermap.org/img/wn/50d@2x.png",
-            temperature: "70",
-          }]
+          forecast: data.daily.reduce((accumulator, day, index) => {
+            if (index === 0 || accumulator.length === 4) {
+              return accumulator
+            }
+            return accumulator.concat({
+              weekday: moment.unix(day.dt).format('ddd'),
+              iconSrc: `http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`,
+              temperature: `${Math.round(day.temp.day)}`,
+            })
+          }, [])
         }
       }
     }
@@ -54,7 +39,6 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
 }
 
 export default function Home(data: InferGetServerSidePropsType<typeof getServerSideProps>) {
-  console.log(data)
   return (
     <>
       <Head>
