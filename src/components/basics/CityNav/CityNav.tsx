c5b932dcabe60c12ab959319ea8951a03d40f4f0
jvandergraaf <jvgraaf@gmail.com>
2022-08-24 19:11:27 -0600

    Feature: navigate to city through link

--- src/components/basics/CityNav/CityNav.tsx
+++ src/components/basics/CityNav/CityNav.tsx
@@ -10,7 +10,7 @@ export const CityNav = ({ cities, selectedCity }: CityNavProps) => (
         <div className="grid grid-cols-3 items-center text-center">
         {cities.map((city, index) => (
             <h1 key={index} className={city === selectedCity ? "arboria-medium-onyx-45px" : "arboria-light-onyx-45px"}>
-                <Link href="/posts/first-post">
+                <Link href={`/${city}`}>
                     {city.toUpperCase()}
                 </Link>
             </h1>)
